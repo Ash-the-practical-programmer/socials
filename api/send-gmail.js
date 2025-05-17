04b35b9c47aab97bfc48dd124a9a7aa5d62fc26e
saatyakkapoor <71920321+saatyakkapoor@users.noreply.github.com>
2025-05-17 09:24:58 +0530

    Update send-gmail.js added title {proper}

--- api/send-gmail.js
+++ api/send-gmail.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
 
   for (const email of emailList) {
     const mailOptions = {
-      from: `"Project Team" <${EMAIL}>`,
+      from: "DeepSocial AI Waitlist!",
       to: email,
       subject: 'You’re on the list ✅',
       html: `
